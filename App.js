67b35f914da972ad3a4590f8f3b8ac7fb9792e09
Sebastian Ballen <sebastian.ballenc@gmail.com>
2021-08-22 15:34:39 -0500

    ➕ADD: Product Images.
    Images added to assets folder to pass it as a prop to card component.

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { View } from 'react-native';
 import ProductList from './app/screens/ProductsList';
 
 export default function App() {
