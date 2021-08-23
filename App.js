646163d1c943b3d15508d6c37062ca6d263c5969
Sebastian Ballen <sebastian.ballenc@gmail.com>
2021-08-22 22:27:51 -0500

    ♻REFACTOR: App js.
    ApplyFilters screen added to file.

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { View } from 'react-native';
+import ApplyFilters from './app/screens/ApplyFilters';
 import ProductList from './app/screens/ProductsList';
 import ShoppingCart from './app/screens/ShoppingCart';
 
 export default function App() {
   return (
     <View>
-      <ShoppingCart />
+      <ApplyFilters />
     </View>
   );
 };
