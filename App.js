325e2a2b77fb3bdbc154c7d0fd3885aa410966b2
Sebastian Ballen <sebastian.ballenc@gmail.com>
2021-08-22 19:21:42 -0500

    ♻REFACTOR: App file.
    App file edited with Shopping Cart component added.

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { View } from 'react-native';
 import ProductList from './app/screens/ProductsList';
+import ShoppingCart from './app/screens/ShoppingCart';
 
 export default function App() {
   return (
     <View>
-      <ProductList />
+      <ShoppingCart />
     </View>
   );
 };
