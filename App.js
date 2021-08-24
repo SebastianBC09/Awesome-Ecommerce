869e3f106e5f14afdcb71ee5cfac1332c34da205
Sebastian Ballen <sebastian.ballenc@gmail.com>
2021-08-24 18:35:36 -0500

    ♻REFACTOR: App file.
    Minor Changes made: Component organization made..

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { View } from 'react-native';
-import ApplyFilters from './app/screens/ApplyFilters';
 import ProductList from './app/screens/ProductsList';
 import ShoppingCart from './app/screens/ShoppingCart';
+import ApplyFilters from './app/screens/ApplyFilters';
 
 export default function App() {
   return (
